08561b8c5cd5cb882e5279ade553fd1a4d523e1c
efer14 <emma.negreido@live.com.ar>
2024-01-01 18:58:06 -0300

    configuracion de envios de mail

---
+++ example-app/resources/views/mail/view.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>MundosE.com</title>
+    </head>
+    <body>
+        <h1>{{ $details['nombre'] }}</h1>
+            <p>{{ $details['apellido'] }}</p>
+            <p>Thank you</p>
+    </body>
+</html>
